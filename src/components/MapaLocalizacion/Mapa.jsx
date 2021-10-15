23481194cc0b1c10311901f06865626d6c267503
yudithkleon <yudithkleon@gmail.com>
2021-10-15 15:35:32 -0500

    pintar en el mapa

--- src/components/MapaLocalizacion/Mapa.jsx
+++ src/components/MapaLocalizacion/Mapa.jsx
@@ -1,63 +1,64 @@
-
 import React, { useState, useEffect } from "react";
-import data from '../../assets/data.json'
-import Markers from './MarcarMapa'
+import data from "../../assets/data.json";
+import Markers from "./MarcarMapa";
 import { useLocation, useHistory } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
-import {  Container } from "react-bootstrap";
-import { MapContainer, TileLayer } from "react-leaflet";
-import '../../style/mapa.css'
-
+import { Container } from "react-bootstrap";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import venues from "../../assets/data.json"
+import "../../style/mapa.css";
+import MarkerPopup from "./MarkerPopup";
 
 export const Mapa = (props) => {
+  // const [state, setState] = useState({
+  //   currentLocation: { lat: 4.6391857, lng: -74.1838036 },
+  //   zoom: 13,
+  //   data,
+  // });
 
-    // const [state, setState] = useState({ 
-    //   currentLocation: {lat: 4.6391857, lng:-74.1838036 },
-    //   zoom: 13,
-    //   data,
-    // });
-  
-    // const location = useLocation();
-    // const history = useHistory();
-  
-    // useEffect(() => {
+  // const location = useLocation();
+  // const history = useHistory();
 
-    //   if (location.state.latitude && location.state.longitude) {
-    //     const currentLocation = {
-    //       lat: location.state.latitude,
-    //       lng: location.state.longitude,
-    //     };
-    //     console.log(state);
-    //     setState({
-    //       ...state,
-    //       data: {
-    //         venues: state.data.venues.concat({
-    //           name: "new",
-    //           geometry: [currentLocation.lat, currentLocation.lng],
-    //         }),
-    //       },
-    //       currentLocation,
-    //     });
-    //     // history.replace({
-    //     //   pathname: "/map",
-    //     //   state: {},
-    //     // });
-    //   }
-    // }, [location]);
-
+  // useEffect(() => {
+  //   if (state.currentLocation.lat && state.currentLocation.lng) {
+  //     const currentLocation = {
+  //       lat: state.currentLocation.lat,
+  //       lng: state.currentLocation.lng,
+  //     };
+  //     console.log(state);
+  //     setState({
+  //       ...state,
+  //       data: {
+  //         venues: state.data.venues.concat({
+  //           name: "new",
+  //           geometry: [currentLocation.lat, currentLocation.lng],
+  //         }),
+  //       },
+  //       currentLocation,
+  //     });
+  //     history.replace({
+  //       pathname: "/map",
+  //       state: {},
+  //     });
+  //   }
+  // }, []);
+  console.log(data.venues[0].geometry);
   return (
     <div>
       <Container>
-      {/* <MapContainer center={state.currentLocation} zoom={state.zoom}> */}
-      <MapContainer center={{lat: 4.6391857, lng:-74.1838036}} zoom={10}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      {/* <Markers venues={state.data.venues} /> */}
-    </MapContainer>
+        {/* <MapContainer center={state.currentLocation} zoom={state.zoom}> */}
+          <MapContainer center={{lat: 4.6391857, lng:-74.1838036}} zoom={15}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          {/* <Markers venues={state.data.venues} /> */}
+         
+          <Marker position={[data.venues[0].geometry[0], data.venues[0].geometry[1]]}>  
+          <MarkerPopup name={data.venues[0].description}/> 
+          </Marker>
+          </MapContainer>
       </Container>
-      </div>
+    </div>
   );
 };
-
--- src/components/MapaLocalizacion/MarkerPopup.jsx
+++ src/components/MapaLocalizacion/MarkerPopup.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { Popup } from "react-leaflet";
 
 const MarkerPopup = (props) => {
-  const { name } = props.data;
   return (
     <Popup>
-      <div>{name}</div>
+      <div>{props.name}</div>
     </Popup>
   );
 };
-
 export default MarkerPopup;
